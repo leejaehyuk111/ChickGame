41880ad046292f80db78162dff038bb5df1d4205
leejaehyuk111 <sheerpeace2@gmail.com>
2024-03-15 23:03:06 +0900

    Ui
    기초유아이 설정

---
+++ Assets/ItemShopButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemShopButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer img1;
+    static int i;
+    public void MoveBackimg()
+    {
+        if (i == 0)
+        {
+            img1.transform.position = new Vector3(0, 0, 0);
+            i = 1;        
+        }
+        else
+        {
+            img1.transform.position += new Vector3(30, 0, 0);
+            i = 0;
+        }
+    }
+}
