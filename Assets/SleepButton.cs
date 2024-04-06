25e64b63d119db43712bf3d58fd833fb1dccac8d
leejaehyuk111 <sheerpeace2@gmail.com>
2024-04-06 18:29:12 +0900

    기능복원

--- Assets/SleepButton.cs
+++ Assets/SleepButton.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class SleepButton : MonoBehaviour
 {
+    public SpriteRenderer img1, img2;
+
     public void OnClickSleep()
     {
+        img1.transform.position = new Vector3(100,0, 0);
+        img2.transform.position = new Vector3(0, -2.27f, 0);
         if (ChickStatus.sleep >= 50)
         {
             ChickStatus.sleep -= 50;
@@ -15,6 +19,14 @@ public class SleepButton : MonoBehaviour
             ChickStatus.sleep = 0;
         }
 
-        Invoke("", 0.5f);
+        StartCoroutine(Wait());
     }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        img1.transform.position = new Vector3(0, -2.27f, 0);
+        img2.transform.position = new Vector3(100, 0, 0);
+    }
+
 }
