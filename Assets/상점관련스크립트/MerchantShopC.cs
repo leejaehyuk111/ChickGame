d445ce7b86fcc3d61c63fa61d90f3244491882c8
leejaehyuk111 <sheerpeace2@gmail.com>
2024-05-28 22:15:53 +0900

    bug

--- Assets/상점관련스크립트/MerchantShopC.cs
+++ Assets/상점관련스크립트/MerchantShopC.cs
@@ -9,5 +9,9 @@ public class MerchantShopC : MonoBehaviour
     public void Close()
     {
         ShopTab.transform.position = new Vector3(0, 1100, 0);
+        ClothShop.iCloth = 0;
+        BackgroundShop.iBackimg = 0;
+        JewelryShop.iJewelry = 0;
+        EatShop.iEat = 0;
     }
 }
